28522b0be763933bcbabb7c3fc304d7e0683bbd9
Meghan La Breche <Megavictoria@gmail.com>
2021-06-06 20:20:55 -0700

    Error Handling

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import classes from './UsersList.module.css';
-import Card from '../UI/card';
+import Card from '../UI/Card';
 
 const UsersList = (props) => {
 	return (
 		<Card className={classes.users}>
 			<ul>
 				{props.users.map((user) => (
-					<li>
+					<li key={user.id}>
 						{user.name} ({user.age} years old)
 					</li>
 				))}
